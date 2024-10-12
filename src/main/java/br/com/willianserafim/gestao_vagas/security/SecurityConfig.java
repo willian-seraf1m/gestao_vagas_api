80ccd3e3cdb3d7756834d9ce7fd172b5df45e18b
willian <willianserafim2017@gmail.com>
2024-10-12 12:34:39 -0300

    feat: implementation JWTProvider and SecurityFilter

--- src/main/java/br/com/willianserafim/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/willianserafim/gestao_vagas/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.willianserafim.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,19 +8,24 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/candidate/*").permitAll()
-                            .requestMatchers("/company/*").permitAll()
+                    auth.requestMatchers("/candidate/register").permitAll()
+                            .requestMatchers("/company/register").permitAll()
                             .requestMatchers("/auth/company").permitAll()
                             .anyRequest().authenticated();
-                });
+                })
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
 
         return http.build();
     }
